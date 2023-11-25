eaff7ca9d75451ecb3a6c75485104e04e997dfa9
Iago <iagodesouza2012@gmail.com>
2023-11-25 18:44:00 -0300

    Alterando o estilo do header e o deixando responsivo

--- portifolio/src/Components/Header/Header.jsx
+++ portifolio/src/Components/Header/Header.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-scroll';
 import { RiMenu3Fill, RiCloseLine } from 'react-icons/ri';
 import './header.css';
 
@@ -6,35 +7,60 @@ function Header() {
     const [showMenu, setShowMenu] = useState(false);
     const logo = "<MeuPortifolio />";
 
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
+
+    const closeMenu = () => {
+        setShowMenu(false);
+    };
+
+    useEffect(() => {
+        document.addEventListener('click', closeMenu);
+
+        return () => {
+            document.removeEventListener('click', closeMenu);
+        };
+    }, []);
+
     return (
         <header>
-            <div className='logo_menu'>
-                <a href="#home">
-                    {logo}
-                </a>
+            <div className='content' onClick={(e) => e.stopPropagation()}>
+                <h2 className='content-logo'>{logo}</h2>
+                <nav className={`content-nav ${showMenu ? 'show' : ''}`}>
+                    <ul>
+                        <li>
+                            <Link to="home" smooth={true} duration={500} onClick={closeMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="projects" smooth={true} duration={500} onClick={closeMenu}>
+                                Projects
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="about" smooth={true} duration={500} onClick={closeMenu}>
+                                About
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="contact" smooth={true} duration={500} onClick={closeMenu}>
+                                Contact
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+                <div className='content-button' onClick={toggleMenu}>
+                    {showMenu ? (
+                        <RiCloseLine size={40} />
+                    ) : (
+                        <RiMenu3Fill size={40} />
+                    )}
+                </div>
             </div>
-            <nav className={`nav_menu ${showMenu ? "hideMenu" : "showMenu"}`}>
-                <a href="#home">
-                    Home
-                </a>
-                <a href="#projects">
-                    Projects
-                </a>
-                <a href="#about">
-                    About
-                </a>
-                <a href="#contact">
-                    Contact
-                </a>
-            </nav>
-            <button
-                className='btn_menu'
-                onClick={() =>
-                    setShowMenu(!showMenu)}>
-                {showMenu ? <RiCloseLine size={40} /> : <RiMenu3Fill size={40} />}
-            </button>
         </header>
-    )
+    );
 }
 
-export default Header
+export default Header;
