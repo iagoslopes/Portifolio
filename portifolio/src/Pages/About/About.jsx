fda04d28f3c03fb816bd86789a1a2ef1d993670f
Iago <iagodesouza2012@gmail.com>
2023-11-26 08:49:47 -0300

    Alterando os icones e colocando palavras em negrito

--- portifolio/src/Pages/About/About.jsx
+++ portifolio/src/Pages/About/About.jsx
@@ -1,7 +1,7 @@
 import { saveAs } from "file-saver";
 import CV_Iago from "../../../public/Iago_CV.pdf";
-import { Quadradinhos, QuadradinhosInverse } from '../../Components/Malha/Quadradinhos'
-import { RiGithubLine, RiLinkedinFill, RiFileList3Line } from "react-icons/ri";
+import { Quadradinhos2, QuadradinhosInverse } from '../../Components/Malha/Quadradinhos'
+import { RiGithubFill, RiLinkedinBoxFill, RiFileList3Line } from "react-icons/ri";
 import './about.css'
 
 function About() {
@@ -11,26 +11,26 @@ function About() {
     return (
         <section id="about">
             <div className='quadradrinhos-about'>
-                <Quadradinhos />
+                <Quadradinhos2 />
             </div>
             <div className="container-about">
                 <div className='about'>
                     <h1 className="title-about">About me</h1>
                     <p className='information'>
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Eu sou Iago e atualmente moro em São Paulo. Cursando Desenvolvimento de Software Multiplataforma na FATEC Mauá, depois de
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Eu sou Iago e atualmente moro em São Paulo. Cursando Desenvolvimento de Software Multiplataforma na <span>FATEC Mauá</span>, depois de
                         muitas tecnologias nas quais eu obtive conhecimento, escolhi a carreira para programação e desenvolvimento frontend. Nos ultimos
                         meses, venho estudando cada vez mais garantindo uma ampla variedade de tecnologias.
                         <br /><br />
                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Em 2023, com apenas alguns meses, venho construindo projetos web e mobile robustos e bonitos, nos quais sou apaixonado. Minha
-                        experiência nas diversas tecnologias frontend são: Ract, HTML, CSS, JavaScript, NodeJS e MongoDB, sempre busco qualidade em tudo o que faço.
+                        experiência nas diversas tecnologias frontend, são elas <span>React, HTML, CSS, JavaScript, NodeJS e MongoDB</span>, sempre busco qualidade em tudo o que faço.
                         <br /><br />
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Você pode me encontrar no GitHub, onde gosto de desenvolver projetos pessoais com React, ou no LinkedIn, onde estou aumento
-                        minha frequência por la. Atualmente estou aprimorando em algumas outras como: TypeScript, SASS e Bootstrap.
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Você pode me encontrar no <span>GitHub</span>, onde gosto de desenvolver projetos pessoais com React, ou no <span>LinkedIn</span>,
+                        onde estou aumento minha frequência por la. Atualmente estou expandindo minhas habilidades estudando <span>TypeScript, SASS e Bootstrap</span>.
                     </p>
                     <div className="about-link">
-                        <a href="https://www.linkedin.com/in/iago-souza-lopes/" target="_BLANK"><RiLinkedinFill size={50} />
+                        <a href="https://www.linkedin.com/in/iago-souza-lopes/" target="_BLANK"><RiLinkedinBoxFill size={50} />
                         </a>
-                        <a href="https://github.com/iagoslopes" target="_BLANK"><RiGithubLine size={50} />
+                        <a href="https://github.com/iagoslopes" target="_BLANK"><RiGithubFill size={50} />
                         </a>
                         <a onClick={handleDownload}><RiFileList3Line size={50} />
                         </a>
